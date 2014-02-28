9061804c4c6d55586e1aa828db7ee1a79df8ec15
Jon Morehouse <morehousej09@gmail.com>
2014-02-27 19:46:54 -0800

    Build out uploader - not currently tested as needed but close

--- ggb/uploader.go
+++ ggb/uploader.go
@@ -1,21 +1,49 @@
 package ggb
 
 import (
+	"strings"
+	"bufio"
 	"io"
 	"sync"
 )
 
-func FileUploader(wg sync.WaitGroup, input io.Reader) {
+func FileUploader(wg * sync.WaitGroup, input io.Reader) {
 		
-	// should manage all the work for an uploading of os.stdin with a list of files (ie: a find command)
-
+	defer wg.Done()
+	reader := bufio.NewReader(input)
+	filePaths := []string{}
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.Trim(line, "\n")
+		if err != nil {
+			break
+		} else {
+			// now we want to pipe this to the processor worker as needed
+			// this functionality can come later
+			filePaths = append(filePaths, line)
+		}
+	}
+	//fileCount := len(filePaths)
+	// we now have all filePaths as strings - begin processing each file
+	var fwg sync.WaitGroup
+	fwg.Add(1)
+	fcomm := make(chan CommunicationOperation)
+	go FileQueueManager(&fwg, fcomm)
+	// now load up and process all the files as needed
+	var pwg sync.WaitGroup
+	pwg.Add(1)
+	go ProcessorManager(&pwg, &filePaths)
+	pwg.Wait()
+	// now go ahead and start up workers as needed
+	var wwg sync.WaitGroup	
+	wcomm := make(chan CommunicationOperation)
+	wwg.Add(1)
+	go Worker(&wwg, wcomm)
 }
 
 func TextUploader(wg sync.WaitGroup, input io.Reader) {
 
 
 }
-
-
 
 
--- ggb/uploader_test.go
+++ ggb/uploader_test.go
@@ -1,19 +1,26 @@
 package ggb
 
 import (
-	"fmt"
+	"io"
 	. "launchpad.net/gocheck"
 )
 
 type UploaderSuite struct {
 	GGBSuite
+	input io.Reader
 }
 
 var _ = Suite(&UploaderSuite{})
 
 func (s *UploaderSuite) SetUpSuite(c *C) {
-
+	
 	s.Bootstrap()
+	// create input as needed
+	reader, err := ReaderFromFilePaths(&s.filePaths)
+	c.Assert(err, IsNil)
+	c.Assert(reader, NotNil)
+	s.input = reader
+	
 }
 
 func (s *UploaderSuite) TearDownSuite(c *C) {
@@ -24,10 +31,10 @@ func (s *UploaderSuite) TearDownSuite(c *C) {
 
 func (s *UploaderSuite) TestUploader(c *C) {
 
-	// 
-	fmt.Println("ASDF")
+	s.wg.Add(1)
+	go FileUploader(&s.wg, s.input)
+	s.wg.Wait()
+
 }
-
-
 
 
